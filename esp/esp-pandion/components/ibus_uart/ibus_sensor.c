a9eb7afc98fcf21321b03dc7888b2afef853c0f7
Alex Raboud <doubar2001@gmail.com>
2020-04-20 19:56:40 -0600

    working on ibus sensors

--- esp/esp-pandion/components/ibus_uart/ibus_sensor.c
+++ esp/esp-pandion/components/ibus_uart/ibus_sensor.c
@@ -1,14 +1,74 @@
 #include "ibus_sensor.h"
 
-esp_err_t ibus_sensor_update(ibus_sensor_vals_t* channel_vals) {
-    int len;
+typedef struct {
+    int sensor_count;
+    uint8_t read_buffer[IBUS_UART_BUFFER_SIZE];
+    ibus_sensor_t* sensors;
+} ibus_sensor_handle_impl;
 
+ibus_sensor_t ibus_create_sensor(ibus_sensor_type_t type, ibus_sensor_value_t value) {
+    ibus_sensor_t sensor = {
+        .sensor_type = type,
+        .value = value
+    };
+
+    return sensor;
+}
+
+esp_err_t ibus_push_sensor(ibus_sensor_handle_t handle, ibus_sensor_t* sensor) {
+    ibus_sensor_handle_impl* handle_impl = (ibus_sensor_handle_impl*) handle;
+    
+    if (handle_impl->sensor_count >= IBUS_MAX_SENSOR_COUNT) {
+        return ESP_FAIL;
+    }
+
+    handle_impl->sensors[handle_impl->sensor_count++] = *sensor;
+
+    return ESP_OK;
+}
+
+esp_err_t parse_sensor_input(ibus_sensor_handle_impl* handle_impl, size_t len) {
+    if (len != 4) {
+        ESP_LOGW(TAG, "Length of %d invalid for IBus sensor.", len);
+        return ESP_FAIL;
+    }
+    
+    esp_err_t ret = ibus_test_checksum(handle_impl->read_buffer);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    uint8_t addr = ret[0] && 0x0f;
+    uint8_t command = ret[0] && 0xf0;
+
+    if (addr > handle_impl->sensor_count)
+
+    switch(command) {
+        case IBUS_CMD_SENSOR_DISCOVER:
+            ESP_LOGD(TAG, "Received sensor discover for %d", addr);
+        case IBUS_CMD_SENSOR_TYPE:
+            ESP_LOGD(TAG, "Received sensor type for %d", addr);
+        case IBUS_CMD_SENSOR_SEND:
+            ESP_LOGD(TAG, "Received sensor send for %d", addr);
+        default:
+            ESP_LOG_W(TAG, "Unsupported IBus sensor command: %x", ret[1]);
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t ibus_sensor_update(ibus_sensor_handle_t handle) {
+    ibus_sensor_handle_impl* handle_impl = (ibus_sensor_handle_impl*) handle;
+    if (handle_impl->sensor_count == 0 ) {
+        return ESP_OK;
+    }
+
+    size_t len;
     ESP_ERROR_CHECK(uart_get_buffered_data_len(CONFIG_IBUS_SENSOR_UART_NUM, (size_t*)&len));
     if (len > 0) {
+        len = uart_read_bytes(CONFIG_IBUS_SENSOR_UART_NUM, handle_impl->read_buffer, IBUS_UART_BUFFER_SIZE, 5 / portTICK_RATE_MS);
 
-        len = uart_read_bytes(CONFIG_IBUS_SENSOR_UART_NUM, read_buffer, IBUS_UART_BUFFER_SIZE, 5 / portTICK_RATE_MS);
-
-        return parse_channel_values(channel_vals, len);
+        return parse_sensor_input(handle);
     }
 
     return ESP_FAIL;
--- esp/esp-pandion/components/ibus_uart/include/ibus_sensor.h
+++ esp/esp-pandion/components/ibus_uart/include/ibus_sensor.h
@@ -1,2 +1,29 @@
 #pragma once
 
+#include "esp_log.h"
+#include "esp_err.h"
+#include "ibus_common.h"
+
+typedef enum {
+    // TODO: this.
+    INTERNAL_POWER = 0x10,
+
+} ibus_sensor_type_t;
+
+typedef uint16_t ibus_sensor_value_t;
+
+typedef struct {
+    ibus_sensor_type_t sensor_type;
+    ibus_sensor_value_t value;
+    uint64_t last_send;
+} ibus_sensor_t;
+
+typedef struct ibus_sensor_handle_impl {
+    
+} *ibus_sensor_handle_t;
+
+ibus_sensor_t ibus_create_sensor(ibus_sensor_type_t type, ibus_sensor_value_t value);
+
+esp_err_t ibus_push_sensor(ibus_sensor_handle_t handle, ibus_sensor_t* sensor);
+
+esp_err_t ibus_sensor_update(ibus_sensor_handle_t handle);

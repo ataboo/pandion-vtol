214a17e5b6f80a0b62c54a0bd24c7457d72db6f9
Alex Raboud <doubar2001@gmail.com>
2020-04-25 23:13:49 -0600

    Adding wifi ap

---
+++ esp/esp-pandion/components/wifi_ap/include/wifi_ap.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+typedef struct wifi_handle_impl *wifi_handle_t;
+
+esp_err_t wifi_ap_init();
